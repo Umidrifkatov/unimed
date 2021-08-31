0fb2711a6bb3816ea0934fc8f82342c5c9a489da
Ваше Имя <you@example.com>
2021-08-31 17:07:38 +0500

    r

--- website/urls.py
+++ website/urls.py
@@ -19,6 +19,9 @@ urlpatterns = [
     path('send_marketing/', views.Markettingmessage),
 ]
 
+
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
